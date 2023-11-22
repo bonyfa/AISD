0b91bcf81e5439a6f17ab00f3a5083d10f7b29d4
Arsen Suleymanian <arsens818@gmail.com>
2023-11-22 13:45:15 +0400

    Добавил операоры + - и оператор []

--- var6exers22.cpp
+++ var6exers22.cpp
@@ -64,6 +64,64 @@ public:
 		delete[] points;
 	}
 
+	void add(const Point<T>& point) {
+		Point<T>* newElements = new Point<T>[size + 1];
+		for (int i = 0; i < size; ++i) {
+			newElements[i] = points[i];
+		}
+		newElements[size] = point;
+		delete[] points;
+		points = newElements;
+		size += 1;
+	}
+
+	Point<T> operator[](const int& index) {
+		if (index < 0 || index >= size) {
+			throw out_of_range("Index out of range");
+		}
+		return points[index];
+	}
+	Polyline<T>& operator+(const Polyline<T>& other) {
+		int newSize = size + other.size;
+		Point<T>* newPoints = new Point<T>[newSize];
+		for (int i = 0; i < size; i++) {
+			newPoints[i] = points[i];
+		}
+		for (int i = 0; i < other.size; i++) {
+			newPoints[size + i] = other.points[i];
+		}
+		delete[] points;
+		points = newPoints;
+		size = newSize;
+		return *this;
+	}
+	//оператор добавляет точку в начало массива
+	Polyline<T>& operator+(const Point<T>& point) {
+		Point<T>* newPoints = new Point<T>[size + 1];
+		newPoints[0] = point;
+		for (int i = 0; i < size; i++) {
+			newPoints[i + 1] = points[i];
+		}
+		delete[] points;
+		points = newPoints;
+		size++;
+		return *this;
+	}
+	//оператор добавляет точку в конец массива
+	Polyline<T>& operator-(const Point<T>& point) {
+		Point<T>* newPoints = new Point<T>[size + 1];
+		for (int i = 0; i < size; i++) {
+			newPoints[i] = points[i];
+		}
+		newPoints[size] = point;
+		delete[] points;
+		points = newPoints;
+		size++;
+		return *this;
+	}
+
+
+
 	friend ostream& operator<<(ostream& os, const Polyline<T>& polyline) {
 		for (int i = 0; i < polyline.size; i++) {
 			os << "(" << polyline.points[i].x << ", " << polyline.points[i].y << ") ";
@@ -153,12 +211,73 @@ public:
 		size += 1;
 	}
 
+	Point<complex<T>>& operator[](int index) {
+		if (index < 0 || index >= size) {
+			throw out_of_range("Index out of range");
+		}
+		return points[index];
+	}
+
+	Polyline<complex<T>>& operator+(const Polyline<complex<T>>& other) {
+		int newSize = size + other.size;
+		Point<complex<T>>* newPoints = new Point<complex<T>>[newSize];
+		for (int i = 0; i < size; i++) {
+			newPoints[i] = points[i];
+		}
+		for (int i = 0; i < other.size; i++) {
+			newPoints[size + i] = other.points[i];
+		}
+		delete[] points;
+		points = newPoints;
+		size = newSize;
+		return *this;
+	}
+	//оператор добавляет точку в начало массива
+	Polyline<complex<T>>& operator+(const Point<complex<T>>& point) {
+		Point<complex<T>>* newPoints = new Point<complex<T>>[size + 1];
+		newPoints[0] = point;
+		for (int i = 0; i < size; i++) {
+			newPoints[i + 1] = points[i];
+		}
+		delete[] points;
+		points = newPoints;
+		size++;
+		return *this;
+	}
+	//оператор добавляет точку в конец массива
+
+	Polyline<complex<T>>& operator-(const Point<complex<T>>& point) {
+		Point<complex<T>>* newPoints = new Point<complex<T>>[size + 1];
+		for (int i = 0; i < size; i++) {
+			newPoints[i] = points[i];
+		}
+		newPoints[size] = point;
+		delete[] points;
+		points = newPoints;
+		size++;
+		return *this;
+	}
+
 
 	friend ostream& operator<<(ostream& os, const Polyline<complex<T>>& polyline) {
 		for (int i = 0; i < polyline.size; i++) {
 			os << "(" << polyline.points[i].x << ",  " << polyline.points[i].y << ") ";
 		}
 		return os;
+	}
+	bool& operator==(const Polyline<complex<T>>& other) {
+		int k = 0;
+
+		if (this->size == other->size)
+			throw out_of_range("size must be =");
+		for (int i = 0; i < size; ++i) {
+			if (this->points[i] == other->points[i])
+				++k;
+		}
+		if (k == size)
+			return true;
+		else
+			return false;
 	}
 
 };
@@ -182,7 +301,27 @@ int main() {
 	cout << "polyline_copy: " << endl;
 	cout << polyline_copy << endl;
 
-	
+	Point<int> new_point(8, 7);
+	polyline_copy.add(new_point);
+	cout << "polyline_add_point: " << endl;
+	cout << polyline_copy << endl;
+
+	cout << "retrieved_point: " << endl;
+	cout << polyline_copy[2] << endl;
+
+	Polyline<int> other_polyline;
+	Polyline<int> combined_polyline = polyline_with_values + polyline_copy;
+	cout << "combined_polyline: " << endl;
+	cout << combined_polyline << endl;
+
+	Polyline<int> point_polyline = combined_polyline - new_point;
+	cout << "point_polyline: " << endl;
+	cout << point_polyline << endl;
+
+	Polyline<int> polyline_point = polyline_copy + new_point;
+	cout << "polyline_point: " << endl;
+	cout << polyline_point << endl;
+
 	cout << "\n----------------------------------------------------" << endl;
 
 	Point<complex<float>> complex_point1(complex<float>(1, 2), complex<float>(3, 4));
@@ -201,5 +340,21 @@ int main() {
 	polyline_copy_complex.add(complex_point2);
 	cout << "polyline_add_complex_point: " << endl;
 	cout << polyline_copy_complex << endl;
+
+	cout << "retrieved_copmlex_point: " << endl;
+	cout << polyline_copy_complex[2] << endl;
+
+	Polyline<complex<float>> combined_complex_polyline = polyline_with_complex_values + polyline_copy_complex;
+	cout << "combined_complex_polyline: " << endl;
+	cout << combined_complex_polyline << endl;
+
+	Polyline<complex<float>> complex_point_polyline = combined_complex_polyline + complex_point2;
+	cout << "complex_point_polyline: " << endl;
+	cout << complex_point_polyline << endl;
+
+	Polyline<complex<float>> polyline_point_complex = combined_complex_polyline + complex_point2;
+	cout << "polyline_point_complex: " << endl;
+	cout << polyline_point_complex << endl;
+
 
 }
